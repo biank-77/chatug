32d7d5ccc523b11dcd6ef41c5885d9e2d6476183
biank-77 <nicol-bianca@hotmail.com>
2025-05-13 01:41:01 -0500

    Create auth services

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3201;
+const PORT = process.env.PORT || 3200;
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en http://localhost:${PORT}`);
